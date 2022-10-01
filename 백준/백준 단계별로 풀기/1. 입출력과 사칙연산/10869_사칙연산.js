4463e148e04f74569374c01d8b039e0f775a02bb
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-10-01 22:39:58 +0900

    사칙연산

--- 백준/백준 단계별로 풀기/1. 입출력과 사칙연산/10869_사칙연산.js
+++ 백준/백준 단계별로 풀기/1. 입출력과 사칙연산/10869_사칙연산.js
@@ -1,7 +1,18 @@
+//방법 1
 let input = require("fs").readFileSync("dev/stdin").toString().split(" ");
 
 const num1 = Number(input[0]);
 const num2 = Number(input[1]);
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(Math.floor(num1 / num2));
+console.log(num1 % num2);
+
+//방법 2
+const fs = require("fs");
+const [a, b] = fs.readFileSync("dev/stdin").toString().split(" ").map(Number);
 
 console.log(num1 + num2);
 console.log(num1 - num2);
