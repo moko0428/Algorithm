fa4c5545ea3e7076b8d1e7996c32202d6838cd0f
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-10-01 23:16:10 +0900

    ??!

--- 백준/백준 단계별로 풀기/1. 입출력과 사칙연산/10869_사칙연산.js
+++ 백준/백준 단계별로 풀기/1. 입출력과 사칙연산/10869_사칙연산.js
@@ -1,21 +1,21 @@
 //방법 1
-let input = require("fs").readFileSync("dev/stdin").toString().split(" ");
+// let input = require("fs").readFileSync("dev/stdin").toString().split(" ");
 
-const num1 = Number(input[0]);
-const num2 = Number(input[1]);
+// const num1 = Number(input[0]);
+// const num2 = Number(input[1]);
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(Math.floor(num1 / num2));
-console.log(num1 % num2);
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(Math.floor(num1 / num2));
+// console.log(num1 % num2);
 
 //방법 2
 const fs = require("fs");
-const [a, b] = fs.readFileSync("dev/stdin").toString().split(" ").map(Number);
+const [a, b] = fs.readFileSync("example.txt").toString().split(" ").map(Number);
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(Math.floor(num1 / num2));
-console.log(num1 % num2);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
--- 백준/백준 단계별로 풀기/1. 입출력과 사칙연산/10998_inputDataMultiplication.js
+++ 백준/백준 단계별로 풀기/1. 입출력과 사칙연산/10998_inputDataMultiplication.js
@@ -1,13 +1,13 @@
-//방법 1
-const fs = require("fs");
-const inputData = fs.readFileSync(0, "utf8").toString().split(" ");
-const A = parseInt(inputData[0]);
-const B = parseInt(inputData[1]);
+// //방법 1
+// const fs = require("fs");
+// const inputData = fs.readFileSync(0, "utf8").toString().split(" ");
+// const A = parseInt(inputData[0]);
+// const B = parseInt(inputData[1]);
 
-console.log(A * B);
+// console.log(A * B);
 
 //방법 2
 const fs = require("fs");
-const [a, b] = fs.readFileSync("dev/stdin").toString().split(" ").map(Number);
+const [a, b] = fs.readFileSync("example.txt").toString().split(" ").map(Number);
 
 console.log(a * b);
