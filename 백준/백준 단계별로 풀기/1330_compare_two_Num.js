4d35441e35e720a242174902d96ce4462211babc
Leejunyoung <ljun925@naver.com>
2022-10-02 20:59:31 +0900

    조건문/두 수 비교하기

---
+++ 백준/백준 단계별로 풀기/1330_compare_two_Num.js
@@ -0,0 +1,9 @@
+const [a,b] = require('fs').readFileSync('dev/stdin').toString().split(' ').map(Number);
+
+if(a>b){
+  console.log('>');
+} else if(a<b){
+  console.log('<');
+} else {
+  console.log('==');
+}
