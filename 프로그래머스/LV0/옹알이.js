56aac9dc2277aa35c59ca000e9ecca902d451c80
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-11-18 23:55:04 +0900

    2022.11.18

---
+++ 프로그래머스/LV0/옹알이.js
@@ -0,0 +1,15 @@
+function solution(babbling) {
+  var answer = 0;
+  for (let i = 0; i < babbling.length; i++) {
+    if (
+      babbling[i]
+        .replace("aya", " ")
+        .replace("ye", " ")
+        .replace("woo", " ")
+        .replace("ma", " ")
+        .trim("") == ""
+    )
+      answer++;
+  }
+  return answer;
+}
